906477856aeeb80043a73f1a205fbfc87126c620
David <davidcha@email.unc.edu>
2023-12-09 01:51:36 -0500

    need to figure out a better way to buffer the read data

--- day-1/day-1.js
+++ day-1/day-1.js
@@ -1,28 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // typescript
-import * as fs from 'fs';
-
-const buf  = Buffer.alloc(8192);
+var fs = require("fs");
+var buf = Buffer.alloc(8192);
 console.log("Advent of Code Day 1!");
-let file = fs.open("./input", "r", function(err, fd: number) {
+var file = fs.open("./input", "r", function (err, fd) {
     if (err) {
         return console.error(err);
     }
     console.log("File opened successfully!");
     console.log("Going to read file");
-    fs.read(fd, buf, 0, buf.length, 0, function(err, bytes: number) {
+    fs.read(fd, buf, 0, buf.length, 0, function (err, bytes) {
         if (err) {
             console.log(err);
         }
         console.log(bytes + " bytes read");
-
         // Print only read bytes to avoid junk.
         if (bytes > 0) {
             console.log(buf.slice(0, bytes).toString());
         }
     });
 });
-
-
-
 console.log("Program Ended");
-export {}
