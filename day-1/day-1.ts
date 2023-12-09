884f5d7583a0c99f7bb0c26b464cd268f42d78ec
David <davidcha@email.unc.edu>
2023-12-09 01:02:58 -0500

    learning how to read files in js

--- day-1/day-1.ts
+++ day-1/day-1.ts
@@ -1,1 +1,6 @@
 // typescript
+const fs = require("fs");
+console.log("Advent of Code Day 1!");
+let data = fs.readFile("./input");
+console.log("Synchronous read: " + data.toString());
+console.log("Program Ended");
